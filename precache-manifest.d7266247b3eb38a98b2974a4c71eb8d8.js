501852a13ea7432e5eb3dcff383eaf66754d8a5c
adhamj <adham-jaafar@outlook.com>
2024-10-20 19:45:16 +0200

    Updates

---
+++ precache-manifest.d7266247b3eb38a98b2974a4c71eb8d8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f0a0d574de0509d40073c38767f309f",
+    "url": "/greenhands/index.html"
+  },
+  {
+    "revision": "aa7d81575e6b2429c3a6",
+    "url": "/greenhands/static/css/main.44e18986.chunk.css"
+  },
+  {
+    "revision": "cd4606195fca5c7a39d2",
+    "url": "/greenhands/static/js/2.a363d009.chunk.js"
+  },
+  {
+    "revision": "5aaa19815932d489dd58",
+    "url": "/greenhands/static/js/3.cb72f06f.chunk.js"
+  },
+  {
+    "revision": "aa7d81575e6b2429c3a6",
+    "url": "/greenhands/static/js/main.2947b586.chunk.js"
+  },
+  {
+    "revision": "71f90e98e9ffef725793",
+    "url": "/greenhands/static/js/runtime~main.2ce722f9.js"
+  },
+  {
+    "revision": "12de7c5246ed7d7db85206350a91d7b9",
+    "url": "/greenhands/static/media/heroImage.12de7c52.jpg"
+  },
+  {
+    "revision": "aeb0bc8990a1b74ab1519a61306e91b7",
+    "url": "/greenhands/static/media/plant 1.aeb0bc89.png"
+  },
+  {
+    "revision": "d802dcc4e34a11404d71c8c55ceafae1",
+    "url": "/greenhands/static/media/plant 2.d802dcc4.png"
+  },
+  {
+    "revision": "01b24a93c32304a2105f826ae892c8c2",
+    "url": "/greenhands/static/media/plant 3.01b24a93.png"
+  }
+]);
